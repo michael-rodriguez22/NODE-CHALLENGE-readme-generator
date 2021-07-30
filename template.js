85b6c03eb7d06ee9c9ffbf01db79ecda081ba945
Michael Rodriguez <mikeyrod22@gmail.com>
2021-07-29 19:42:54 -0500

    placeholder template structure in place

---
+++ template.js
@@ -0,0 +1,31 @@
+const template = `
+# Your Project Title
+
+## Description 
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage) 
+* [Credits](#Credits)
+* [License](#License)
+* [Badges](#Badges)
+* [Features](#Features)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+
+## Installation
+
+## Usage 
+
+## Credits
+
+## License
+
+## Badges
+
+## Features
+
+## Contributing
+
+## Tests
+`
